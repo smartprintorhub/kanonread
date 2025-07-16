025193c26597ecbe701f811cf53f0a5eeb6d345c
smartprintorhub <nikitadigiconnect19@gmail.com>
2025-07-16 13:26:36 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'setup Canon Printer'
+project = 'setup Kanon Printer'
 
 
 # The full version, including alpha/beta/rc tags
